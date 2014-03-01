8c2a3ab9a87b26289f007ad35f37deefc754bb9d
codelittle <igorputina@gmail.com>
2014-03-01 14:50:01 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Summithike::Application.config.secret_key_base = '80d5133380cd3a46b1b5d6bb609b92504eea2d9395a7c0396cc1859be16e1656b2a6ea040fdbb367f25606341e0cc795ac31eba0e02ab5604f152f147c1b69c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Summithike::Application.config.session_store :cookie_store, key: '_summithike_session'
