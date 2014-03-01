3b68c830ec72f37fb841697e79ae652122bd4b3d
codelittle <igorputina@gmail.com>
2014-03-01 18:40:26 +0200

    Views, improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    self.email
+  end
 end
